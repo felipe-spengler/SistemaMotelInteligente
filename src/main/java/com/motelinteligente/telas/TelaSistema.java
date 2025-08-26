ca17b86a9e3ff7e89de8c3caebd26b5d33c17892
Felipe Spengler <felipinhospengler@gmail.com>
2025-08-26 11:48:19 -0300

    123

--- src/main/java/com/motelinteligente/telas/TelaSistema.java
+++ src/main/java/com/motelinteligente/telas/TelaSistema.java
@@ -17,7 +17,7 @@ import org.jsoup.nodes.Element;
 
 public class TelaSistema {
 
-    private static final String REMOTE_JAR_URL = "http://motelinteligente.com/AbelardoJar/";
+    private static final String REMOTE_JAR_URL = "http://motelinteligente.com/ToledoJar/";
 
     public static void main(String[] args) {
         try {
